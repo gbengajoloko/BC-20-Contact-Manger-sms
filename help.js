430df8a354759275d322aa150e734928d788c32d
gbengajoloko <gbengajoloko@gmail.com>
2017-03-23 22:53:08 +0100

    added help view

---
+++ help.js
@@ -0,0 +1,21 @@
+var Table = require('easy-table')
+ 
+var data = [
+  { id: 'When using for the first time', desc: 'npm install'},
+  { id: 'To add contact', desc: 'node add -n <name> -p <phone number>'},
+  { id: 'To view contact ', desc: 'node view'},
+  { id: 'To Delete contact ', desc: 'node delete <name>'},
+  { id: 'To search for contact', desc: 'node search <name>'},
+  { id: 'To send SMS to contact', desc: 'node text <name> -m \'message to be sent\''},
+  { id: 'To view help', desc: 'node help'}
+]
+ 
+var t = new Table
+ 
+data.forEach(function(product) {
+  t.cell('Functions', product.id)
+  t.cell('Instruction', product.desc)
+  t.newRow()
+})
+ 
+console.log(t.toString())
